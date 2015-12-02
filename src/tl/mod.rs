f2040c13b844bfe564c4484fe31949e4ce0fb9ee
Connor Hilarides <connorcpu@live.com>
2015-12-01 23:58:01 -0800

    Read/Write byte arrays too!

--- src/tl/mod.rs
+++ src/tl/mod.rs
@@ -1,5 +1,6 @@
 use std::io::{Read, Write};
 use tl::parsing::{ConstructorId, ReadContext, WriteContext};
+use byteorder::{ByteOrder, LittleEndian, ReadBytesExt, WriteBytesExt};
 
 pub use self::error::{Error, Result};
 #[doc(inline)]
@@ -69,3 +70,75 @@ impl_tl_primitive! { i32, read_i32, write_i32 }
 impl_tl_primitive! { i64, read_i64, write_i64 }
 impl_tl_primitive! { f64, read_f64, write_f64 }
 
+impl<'a> Type for &'a [u8] {
+    fn bare_type() -> bool {
+        true
+    }
+    
+    fn type_id(&self) -> Option<ConstructorId> {
+        None
+    }
+    
+    fn serialize<W: Write>(&self, writer: &mut WriteContext<W>) -> Result<()> {
+        let len = self.len();
+        assert!(len & 0xFF000000 == 0); // len fits in a 24-bit integer
+        
+        // Writing string length is WAT
+        if len <= 253 {
+            try!(writer.write_u8(len as u8));
+        } else {
+            let mut buf = [254; 5];
+            LittleEndian::write_u32(&mut buf[1..], len as u32);
+            try!(writer.write_all(&buf[0..4]));
+        }
+        
+        // Write the actual string and padding
+        try!(writer.write_all(*self));
+        try!(writer.pad(4));
+        
+        Ok(())
+    }
+    
+    fn deserialize<R: Read>(_: &mut ReadContext<R>) -> Result<Self> {
+        Err(Error::ReceivedSendType)
+    }
+    
+    fn deserialize_boxed<R: Read>(_: ConstructorId, _: &mut ReadContext<R>) -> Result<Self> {
+        Err(Error::ReceivedSendType)
+    }
+}
+
+impl<'a> Type for Vec<u8> {
+    fn bare_type() -> bool {
+        true
+    }
+    
+    fn type_id(&self) -> Option<ConstructorId> {
+        None
+    }
+    
+    fn serialize<W: Write>(&self, writer: &mut WriteContext<W>) -> Result<()> {
+        (&self[..]).serialize(writer)
+    }
+    
+    fn deserialize<R: Read>(reader: &mut ReadContext<R>) -> Result<Self> {
+        let low_len = try!(reader.read_u8());
+        let len = if low_len != 254 {
+            low_len as usize
+        } else {
+            let mut buf = [0; 4];
+            try!(reader.read_exact(&mut buf[0..3]));
+            LittleEndian::read_u32(&buf) as usize
+        };
+        
+        let mut bytes = vec![0; len];
+        try!(reader.read_exact(&mut bytes));
+        try!(reader.align(4));
+        
+        Ok(bytes)
+    }
+    
+    fn deserialize_boxed<R: Read>(_: ConstructorId, _: &mut ReadContext<R>) -> Result<Self> {
+        Err(Error::PrimitiveAsPolymorphic)
+    }
+}
--- src/tl/string.rs
+++ src/tl/string.rs
@@ -1,8 +1,7 @@
 use std::string;
 use std::io::{Read, Write};
-use tl::{self, Type, WriteHelpers, ReadHelpers};
+use tl::{self, Type};
 use tl::parsing::{ConstructorId, ReadContext, WriteContext};
-use byteorder::{ByteOrder, LittleEndian, ReadBytesExt, WriteBytesExt};
 
 pub struct String(pub string::String);
 pub struct SendStr<'a>(pub &'a str);
@@ -33,19 +32,7 @@ impl<'a> Type for String {
     }
     
     fn deserialize<R: Read>(reader: &mut ReadContext<R>) -> tl::Result<Self> {
-        let low_len = try!(reader.read_u8());
-        let len = if low_len != 254 {
-            low_len as usize
-        } else {
-            let mut buf = [0; 4];
-            try!(reader.read_exact(&mut buf[0..3]));
-            LittleEndian::read_u32(&buf) as usize
-        };
-        
-        let mut bytes = vec![0; len];
-        try!(reader.read_exact(&mut bytes));
-        try!(reader.align(4));
-        
+        let bytes = try!(reader.read_bare());
         Ok(String(try!(string::String::from_utf8(bytes))))
     }
     
@@ -64,22 +51,7 @@ impl<'a> Type for SendStr<'a> {
     }
     
     fn serialize<W: Write>(&self, writer: &mut WriteContext<W>) -> tl::Result<()> {
-        let len = self.0.len();
-        assert!(len & 0xFF000000 == 0); // len fits in a 24-bit integer
-        
-        // Writing string length is WAT
-        if len <= 253 {
-            try!(writer.write_u8(len as u8));
-        } else {
-            let mut buf = [254; 5];
-            LittleEndian::write_u32(&mut buf[1..], len as u32);
-            try!(writer.write_all(&buf[0..4]));
-        }
-        
-        // Write the actual string and padding
-        try!(writer.write_all(self.0.as_bytes()));
-        try!(writer.pad(4));
-        
+        try!(writer.write_bare(&self.0.as_bytes()));
         Ok(())
     }
     
