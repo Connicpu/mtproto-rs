2f469a48fc0a411d3ad84ea60497139ebaeec327
Connor Hilarides <connorcpu@live.com>
2015-12-07 22:20:10 -0800

    1 more tweak

--- src/rpc/mod.rs
+++ src/rpc/mod.rs
@@ -4,6 +4,7 @@ pub struct Session {
     session_id: u64,
     server_salt: u64,
     message_id_seq: u16,
+    message_id_last_nano: u16,
 }
 
 impl Session {
@@ -11,12 +12,21 @@ impl Session {
         let time = UTC::now();
         let timestamp = time.timestamp();
         let nano = time.nanosecond();
-        let id = self.message_id_seq;
-        self.message_id_seq += 1;
         
+        let nano_bits = (nano >> 14) as u16 & 0xFFFC;
+        let id = if nano_bits == self.message_id_last_nano {
+            let id = self.message_id_seq;
+            self.message_id_seq += 1;
+            id
+        } else {
+            self.message_id_seq = 0;
+            0
+        };
+        
+        // [ lower 32-bits of unix time | bits 13..30 of the nanosecond (14 total) | id | 0b00 ]
         ((timestamp as u64) << 32) |
-        (((id as u64) << 16) & (0xFFFF0000)) |
-        (((nano as u64) >> 14) & (0xFFFF ^ 0b11))
+        ((nano_bits as u64) << 16)
+        ((id as u64) << 2)
     }
 }
 
