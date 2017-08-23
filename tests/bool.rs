8c8b703a994cb9d1b9eefbe623fef32fb86336f0
hcpl <hcpl.prog@gmail.com>
2017-08-23 19:27:24 +0300

    Update tests to the current state of the library

--- tests/bool.rs
+++ tests/bool.rs
@@ -2,9 +2,9 @@ extern crate mtproto;
 extern crate byteorder;
 
 use std::io::Cursor;
-use mtproto::tl::Bool;
-use mtproto::tl::parsing::{ReadContext, WriteContext};
-use byteorder::{ByteOrder, LittleEndian, WriteBytesExt};
+use mtproto::schema::Bool;
+use mtproto::tl::parsing::{Reader, ReadContext, Writer, WriteContext};
+use byteorder::{ByteOrder, LittleEndian};
 
 #[derive(Default)]
 struct BoolTest {
@@ -26,10 +26,10 @@ fn bool_serialization() {
     let mut buffer: [u8; 4] = [0; 4];
     let correct = BoolTest::new();
 
-    WriteContext::new(Cursor::new(&mut buffer[..])).write_generic(&Bool(true)).unwrap();
+    WriteContext::new(Cursor::new(&mut buffer[..])).write_tl(&Bool::boolTrue).unwrap();
     assert_eq!(buffer, correct.true_buffer);
 
-    WriteContext::new(Cursor::new(&mut buffer[..])).write_generic(&Bool(false)).unwrap();
+    WriteContext::new(Cursor::new(&mut buffer[..])).write_tl(&Bool::boolFalse).unwrap();
     assert_eq!(buffer, correct.false_buffer);
 }
 
@@ -37,9 +37,15 @@ fn bool_serialization() {
 fn bool_deserialization() {
     let data = BoolTest::new();
 
-    let true_value: Bool = ReadContext::new(Cursor::new(&data.true_buffer[..])).read_generic().unwrap();
-    assert_eq!(true_value, Bool(true));
+    let true_value: Bool = ReadContext::new(Cursor::new(&data.true_buffer[..])).read_tl().unwrap();
+    match true_value {
+        Bool::boolTrue => {},
+        other => panic!("Bool::boolTrue deserialization failed: actual value {:?}", other),
+    }
 
-    let false_value: Bool = ReadContext::new(Cursor::new(&data.false_buffer[..])).read_generic().unwrap();
-    assert_eq!(false_value, Bool(false));
+    let false_value: Bool = ReadContext::new(Cursor::new(&data.false_buffer[..])).read_tl().unwrap();
+    match false_value {
+        Bool::boolFalse => {},
+        other => panic!("Bool::boolFalse deserialization failed: actual value {:?}", other),
+    }
 }
