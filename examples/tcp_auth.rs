0825c525e1fb971892e60fb4b5f83c436e7c1e49
hcpl <hcpl.prog@gmail.com>
2017-09-30 02:52:11 +0300

    Add TCP authorization key example

---
+++ examples/tcp_auth.rs
@@ -0,0 +1,229 @@
+extern crate byteorder;
+extern crate env_logger;
+#[macro_use]
+extern crate error_chain;
+extern crate futures;
+extern crate log;
+extern crate mtproto;
+extern crate rand;
+extern crate serde;
+extern crate serde_mtproto;
+extern crate tokio_core;
+extern crate tokio_io;
+extern crate toml;
+
+
+use std::fs;
+use std::io::{self, Read};
+
+use byteorder::{BigEndian, ByteOrder, LittleEndian};
+use futures::Future;
+use mtproto::rpc::{AppId, Session};
+use mtproto::rpc::message::{Message, MessageType};
+use mtproto::rpc::encryption::asymm;
+use mtproto::schema;
+use rand::{Rng, ThreadRng};
+use serde::Serialize;
+use serde_mtproto::{Identifiable, MtProtoSized};
+use tokio_core::net::TcpStream;
+use tokio_core::reactor::{Core, Handle};
+
+
+mod error {
+    error_chain! {
+        links {
+            MtProto(::mtproto::Error, ::mtproto::ErrorKind);
+            SerdeMtProto(::serde_mtproto::Error, ::serde_mtproto::ErrorKind);
+        }
+
+        foreign_links {
+            Io(::std::io::Error);
+            SetLogger(::log::SetLoggerError);
+            TomlDeserialize(::toml::de::Error);
+        }
+    }
+}
+
+use error::ResultExt;
+
+macro_rules! tryf {
+    ($e:expr) => {
+        match { $e } {
+            Ok(v) => v,
+            Err(e) => return Box::new(futures::future::err(e.into())),
+        }
+    }
+}
+
+
+// For now only use ABRIDGED protocol
+fn auth(handle: Handle) -> Box<Future<Item = (), Error = error::Error>> {
+    let app_id = tryf!(load_app_id());
+
+    let remote_addr = "149.154.167.51:443".parse().unwrap();
+    println!("Address: {:?}", &remote_addr);
+    let socket = TcpStream::connect(&remote_addr, &handle).map_err(error::Error::from);
+
+    let process = socket.and_then(|socket| {
+        let mut rng = rand::thread_rng();
+        let mut session = Session::new(rng.gen(), app_id);
+
+        let req_pq = schema::rpc::req_pq {
+            nonce: rng.gen(),
+        };
+
+        let request = create_tcp_request(&mut session, socket, req_pq, MessageType::PlainText, true);
+
+        request.map(|(s, b)| (s, b, session, rng))
+    }).and_then(|(socket, response_bytes, mut session, mut rng)|
+        -> Box<Future<Item = (TcpStream, Vec<u8>, Session, ThreadRng), Error = error::Error>>
+    {
+        println!("Response bytes: {:?}", &response_bytes);
+        let response: Message<schema::ResPQ> = tryf!(session.process_message(&response_bytes));
+        println!("Message received: {:#?}", &response);
+
+        let res_pq = match response {
+            Message::PlainText { body, .. } => body.into_inner().into_inner(),
+            _ => unreachable!(),
+        };
+
+        // FIXME: check nonces' equality here
+
+        let pq_u64 = BigEndian::read_u64(&res_pq.pq);
+        println!("Decomposing pq = {}...", pq_u64);
+        let (p_u32, q_u32) = tryf!(asymm::decompose_pq(pq_u64));
+        println!("Decomposed p = {}, q = {}", p_u32, q_u32);
+        let u32_to_vec = |num| {
+            let mut v = vec![0; 4];
+            BigEndian::write_u32(v.as_mut_slice(), num);
+            v
+        };
+        let p = u32_to_vec(p_u32);
+        let q = u32_to_vec(q_u32);
+
+        let p_q_inner_data = schema::P_Q_inner_data {
+            pq:  res_pq.pq,
+            p: p.clone().into(),
+            q: q.clone().into(),
+            nonce: res_pq.nonce,
+            server_nonce: res_pq.server_nonce,
+            new_nonce: rng.gen(),
+        };
+
+        println!("Data to send: {:#?}", &p_q_inner_data);
+        let p_q_inner_data_serialized = tryf!(serde_mtproto::to_bytes(&p_q_inner_data));
+        println!("Data bytes to send: {:?}", &p_q_inner_data_serialized);
+        let known_sha1_fingerprints = tryf!(asymm::KNOWN_RAW_KEYS.iter()
+            .map(|raw_key| {
+                let sha1_fingerprint = raw_key.read()?.sha1_fingerprint()?;
+                Ok(sha1_fingerprint.iter().map(|b| format!("{:02x}", b)).collect::<String>())
+            })
+            .collect::<error::Result<Vec<_>>>());
+        println!("Known public key SHA1 fingerprints: {:?}", known_sha1_fingerprints);
+        let known_fingerprints = tryf!(asymm::KNOWN_RAW_KEYS.iter()
+            .map(|raw_key| Ok(raw_key.read()?.fingerprint()?))
+            .collect::<error::Result<Vec<_>>>());
+        println!("Known public key fingerprints: {:?}", known_fingerprints);
+        let server_pk_fingerprints = res_pq.server_public_key_fingerprints.inner().as_slice();
+        println!("Server public key fingerprints: {:?}", &server_pk_fingerprints);
+        let (rsa_public_key, fingerprint) =
+            tryf!(asymm::find_first_key_fail_safe(server_pk_fingerprints));
+        println!("RSA public key used: {:#?}", &rsa_public_key);
+        let encrypted_data = tryf!(rsa_public_key.encrypt(&p_q_inner_data_serialized));
+        println!("Encrypted data: {:?}", encrypted_data.as_ref());
+
+        let req_dh_params = schema::rpc::req_DH_params {
+            nonce: res_pq.nonce,
+            server_nonce: res_pq.server_nonce,
+            p: p.into(),
+            q: q.into(),
+            public_key_fingerprint: fingerprint,
+            encrypted_data: encrypted_data.to_vec().into(),
+        };
+
+        let request = create_tcp_request(&mut session, socket, req_dh_params, MessageType::PlainText, false);
+
+        Box::new(request.map(|(s, b)| (s, b, session, rng)))
+    }).and_then(|(_socket, response_bytes, session, _rng)| {
+        println!("Response bytes: {:?}", &response_bytes);
+        let response: Message<schema::Server_DH_Params> = tryf!(session.process_message(&response_bytes));
+        println!("Message received: {:#?}", &response);
+
+        Box::new(futures::future::ok(()))
+    });
+
+    Box::new(process)
+}
+
+fn load_app_id() -> error::Result<AppId> {
+    let mut config_data = String::new();
+    let mut file = fs::File::open("app_id.toml")
+        .chain_err(|| "this example needs a app_id.toml file with `api_id` and `api_hash` fields in it")?;
+
+    file.read_to_string(&mut config_data)?;
+    let app_id = toml::from_str(&config_data)?;
+
+    Ok(app_id)
+}
+
+fn create_tcp_request<T>(session: &mut Session,
+                         socket: TcpStream,
+                         data: T,
+                         message_type: MessageType,
+                         is_first_request: bool)
+                        -> Box<Future<Item = (TcpStream, Vec<u8>), Error = error::Error>>
+    where T: ::std::fmt::Debug + Serialize + Identifiable + MtProtoSized
+{
+    let message = tryf!(session.create_message(data, message_type));
+    println!("Message to send: {:#?}", &message);
+    let serialized_message = tryf!(serde_mtproto::to_bytes(&message));
+    println!("Request bytes: {:?}", &serialized_message);
+
+    let mut data = if is_first_request { vec![0xef] } else { vec![] };
+    let len = serialized_message.len() / 4;
+
+    if len < 0x7f {
+        data.push(len as u8);
+    } else if len < 0xff_ff_ff {
+        data.push(0x7f);
+        LittleEndian::write_uint(&mut data, len as u64, 3); // Use safe cast here
+    } else {
+        panic!("Message of length {} too long to send");
+    }
+
+    data.extend(serialized_message);
+    let request = tokio_io::io::write_all(socket, data);
+
+    let response = request.and_then(|(socket, _request_bytes)| {
+        tokio_io::io::read_exact(socket, [0; 1])
+    }).and_then(|(socket, byte_id)| {
+        let boxed: Box<Future<Item = (TcpStream, usize), Error = io::Error>> = if byte_id == [0x7f] {
+            Box::new(tokio_io::io::read_exact(socket, [0; 3]).map(|(socket, bytes_len)| {
+                let len = LittleEndian::read_uint(&bytes_len, 3) as usize * 4;
+                (socket, len)
+            }))
+        } else {
+            Box::new(futures::future::ok((socket, byte_id[0] as usize * 4)))
+        };
+
+        boxed
+    }).and_then(|(socket, len)| {
+        tokio_io::io::read_exact(socket, vec![0; len])
+    });
+
+    Box::new(response.map_err(Into::into))
+}
+
+
+
+fn run() -> error::Result<()> {
+    env_logger::init()?;
+    let mut core = Core::new()?;
+
+    let auth_future = auth(core.handle());
+    core.run(auth_future)?;
+
+    Ok(())
+}
+
+quick_main!(run);
